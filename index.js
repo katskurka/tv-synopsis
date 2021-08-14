f5a8ebc54c236cfb8b63b433de2e1b3aa2f58948
katskurka <kati.skurka@gmail.com>
2021-08-14 00:10:28 -0400

    set up home route, not working yet because I forgot engine

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+/* eslint-disable no-console */
 const express = require('express')
 const app = express()
 
 const showData = require('./showdata')
+
+// setup port
+app.listen(1337, () => {
+  console.log('Server is up!')
+})
+
+// setting up home
+app.get('/', (req, res) => {
+  res.render('index', { showData })
+})
+
+
