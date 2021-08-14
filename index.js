1245017d162cbab2e1bf8ac05b265e38432558c0
katskurka <kati.skurka@gmail.com>
2021-08-14 00:54:11 -0400

    setup server skeleton and added views folder with pug files and public css files

--- index.js
+++ index.js
@@ -9,9 +9,24 @@ app.listen(1337, () => {
   console.log('Server is up!')
 })
 
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+
 // setting up home
 app.get('/', (req, res) => {
   res.render('index', { showData })
+})
+
+// getting seasons
+app.get('/seasons/:id', (req, res) => {
+  const showSeason = showData.seasons.filter(season => season.number === parseInt(req.params.id))
+
+  return res.render('seasons', { showSeason })
+})
+
+app.all('*', (req, res) => {
+  return res.status(404).send('Negative, Captain')
 })
 
 
